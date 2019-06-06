36e55108c9921f73960989c4abd4ec3d99c2134d
Brian H <brianbhoang9@gmail.com>
2019-06-06 16:47:41 -0700

    Messing around w/ newline fix

--- doc_fix.py
+++ doc_fix.py
@@ -23,22 +23,29 @@ class Window(Frame):
 		#buttons
 		showButton = Button(self, text="Show", command = lambda: self.print_entry(entry.get()))
 
-		insertButton = Button(self, text="Insert Bottom", 
+		insertBottomButton = Button(self, text="Top to Bottom", 
 			command = lambda: self.insert_tBox(afterTextBox, beforeTextBox.get("1.0", END)))
 
-		deleteButton = Button(self, text="Clear Top", command = lambda: self.clear_tBox(beforeTextBox))
+		insertTopButton = Button(self, text="Bottom to Top",
+			command = lambda: self.insert_tBox(beforeTextBox, afterTextBox.get("1.0", END)))
+
+		deleteButton = Button(self, text="Clear Top", 
+			command = lambda: self.clear_tBox(beforeTextBox))
 
 		bulletsButton = Button(self, text="Fix bullets", 
 			command = lambda: self.fix_bullets(afterTextBox,beforeTextBox.get("1.0", END)))
 
-		fixEspButton = Button(self, text="Convert Spanish",
-			command = lambda: self.convert_esp(afterTextBox,beforeTextBox.get("1.0", END)))
+		fixSpecial = Button(self, text="Fix Special Char",
+			command = lambda: self.convert_special(afterTextBox,beforeTextBox.get("1.0", END)))
 
-		asciiButton = Button(self, text="Get ascii",
+		asciiButton = Button(self, text="Get ASCII",
 			command = lambda: self.get_ascii(afterTextBox,beforeTextBox.get("1.0", END)))
 
 		newlineButton = Button(self, text="Fix newline",
 			command = lambda: self.fix_newline(afterTextBox,beforeTextBox.get("1.0", END)))
+
+		createListButton = Button(self, text="Create list",
+			command = lambda: self.create_list(afterTextBox,beforeTextBox.get("1.0", END)))
 
 		exitButton = Button(self, text="Exit",
 			command = self.client_exit)
@@ -52,22 +59,21 @@ class Window(Frame):
 		afterTextBox.pack()
 
 		file.add_command(label="Exit", command=self.client_exit)
-		edit.add_command(label="Show Text", command=self.showText)
-
 		menu.add_cascade(label="File", menu=file)
-		menu.add_cascade(label = "Edit", menu=edit)
 
 		beforeTextBox.place(x=150,y=10)
 		afterTextBox.place(x=150,y=300)
 
 		#placing buttons
-		insertButton.place(x=10,y=10)
-		deleteButton.place(x=10,y=40)
-		bulletsButton.place(x=10,y=70)
-		fixEspButton.place(x=10, y=100)
-		asciiButton.place(x=10, y=130)
-		newlineButton.place(x=10, y=160)
-		exitButton.place(x=10, y=190)
+		insertBottomButton.place(x=10,y=10)
+		insertTopButton.place(x=10,y=40)
+		deleteButton.place(x=10,y=70)
+		bulletsButton.place(x=10,y=100)
+		fixSpecial.place(x=10, y=130)
+		asciiButton.place(x=10, y=160)
+		newlineButton.place(x=10, y=190)
+		createListButton.place(x=10, y=230)
+		exitButton.place(x=10, y=260)
 
 	def showText(self):
 		text = Label(self, text="Greetings")
@@ -79,6 +85,7 @@ class Window(Frame):
 	def print_entry(self, newString):
 		print(newString)
 
+	#Insert text into a textBox
 	def insert_tBox(self, tBox, nString):
 		self.clear_tBox(tBox)
 		tBox.insert(END, nString)
@@ -106,20 +113,54 @@ class Window(Frame):
 		self.clear_tBox(tBox)
 		tBox.insert(END, writeStr)
 
-	#convert Spanish characters into their HTML equivalent
-	def convert_esp(self, tBox, nString):
+	#Create an HTML list w/ lines separated by "\n"
+	def create_list(self, tBox, nString):
+		writeStr = "<ul><li>"
+
+		for c in nString:
+			if(ord(c) == 10):
+				writeStr += "</li><li>"
+
+			else:
+				writeStr += c
+
+		writeStr += "</li></ul>"
+
+		self.clear_tBox(tBox)
+		tBox.insert(END, writeStr)
+
+	#convert Spanish and unicode punctuation into their HTML and ASCII 
+	#equivalent
+	def convert_special(self, tBox, nString):
 		writeStr = ""
 
 		specialCharDict = {193:"&#193;", 201:"&#201;", 205:"&#205;", 211:"&#211;", 
 					218:"&#218;",209: "&#209;", 220: "&#220;", 225: "&#225;", 
 					233: "&#233;",237:"&#237;", 243: "&#243;",250:"&#250;", 
-					241: "&#241;", 252: "&#252;", 191: "&#191;", 161: "&#161;"}
+					241: "&#241;", 252: "&#252;", 191: "&#191;", 161: "&#161;",
+					8220: '"', 8221: '"', 8217: "'"}
 
+		newline = False
+
+		#Some Spanish characters are interpreted as a regular letter with the
+		#accent mark added to them; ex: 'Small latin "o" with acute'
+		#These are read as a string b/c the letter and the accompanying
+		#accent mark are read separately, but are printed seemingly together
+
+#					{ 65 769: "&#193;", 69 769:"&#201;",
+#					73 769:"&#205;", 79 769:"&#211;", 85 769:"&#218;", 78 771: "&#209;", 
+#					85 776: "&#220;", 97 769: "&#225;", 101 769: "&#233;", 105 769:"&#237;", 
+#					111 769: "&#243;", 117 769:"&#250;", 110 771: "&#241;", 117 776: "&#252;"}
+
+		#check if Char exists in dictionary of special characters
 		for c in nString:
-			if(ord(c) in specialCharDict):
-				writeStr += specialCharDict[ord(c)]
-			else:
-				writeStr += c
+
+#			if(ord(c) in specialCharDict):
+#				writeStr += specialCharDict[ord(c)]
+#			else:
+#				writeStr += c
+
+			writeStr += specialCharDict.get(ord(c), c)
 
 		self.clear_tBox(tBox)
 		tBox.insert(END, writeStr)
@@ -145,12 +186,16 @@ class Window(Frame):
 	def fix_newline(self, tBox, nString):
 
 		writeStr = ""
+		newline = False
 
 		for c in nString:
-			if(ord(c) == 10):
-				writeStr += " "
+			#if(ord(c) == 10):
+			#	writeStr += " "
 
+			if(ord(c) == 10) and (newline == False):
+				newline = True
 			else:
+				newline = False
 				writeStr += c
 
 
@@ -158,9 +203,7 @@ class Window(Frame):
 		tBox.insert(END, writeStr)
 
 root = Tk()
-
 root.geometry("800x600")
-
 app = Window(root)
-
+root.configure(background="grey")
 root.mainloop()
