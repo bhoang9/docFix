e74c0278a23e1af45f16d694d9a8aa9c8cfe0441
YAPI-NB-009 <yapi-nb-009@YAPINB009siMac.localdomain>
2019-06-17 09:27:01 -0700

    figured out passing textbox

--- textbox_panel.py
+++ textbox_panel.py
@@ -1,8 +1,5 @@
 from tkinter import *
 
-beforeTextBox = Text()
-afterTextBox = Text()
-
 class textbox_panel:
 
 	def __init__(self, root, frame):
@@ -10,21 +7,21 @@ class textbox_panel:
 		self.root = root
 		self.frame = frame
 
-		#self.beforeTextBox = None
-		#self.afterTextBox = None
+		self.beforeTextBox = None
+		self.afterTextBox = None
 
 		self.init_textboxes()
 
 	def get_before_textbox(self):
-		return beforeTextBox
+		return self.beforeTextBox
 
 	def get_after_textbox(self):
-		return afterTextBox
+		return self.afterTextBox
 
 	def init_textboxes(self):
 
-		beforeTextBox = Text(self.frame, height=18,width=80, borderwidth=2, relief="solid")
-		afterTextBox = Text(self.frame, height=18, width=80, borderwidth=2, relief="solid")
+		self.beforeTextBox = Text(self.frame, height=18,width=80, borderwidth=2, relief="solid")
+		self.afterTextBox = Text(self.frame, height=18, width=80, borderwidth=2, relief="solid")
 
-		beforeTextBox.pack(side = TOP, fill = BOTH, expand = True)
-		afterTextBox.pack(side = BOTTOM, fill = BOTH, expand = True)
+		self.beforeTextBox.pack(side = TOP, fill = BOTH, expand = True)
+		self.afterTextBox.pack(side = BOTTOM, fill = BOTH, expand = True)
